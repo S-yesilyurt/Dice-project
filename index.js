c361a0f4ab078bbf6c7b8dced8ed514bbec35836
S-yesilyurt <87809752+S-yesilyurt@users.noreply.github.com>
2021-08-08 14:46:00 +0300

    Dice

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() *6) + 1;
+var randomDice = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomDice);
+
+
+
+
+var randomNumber2 = Math.floor(Math.random() *6) +1 ;
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomDice2);
+
+
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = " ✔ Player 1 Win";
+}
+else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Win ✔";
+}
+
+else{
+  document.querySelector("h1").innerHTML ="DRAW ‼";
+}
